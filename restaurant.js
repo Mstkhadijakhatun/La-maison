e7b79c92e6f455735425c982d4baf1923e2cdef8
Mstkhadijakhatun <khadijajoadder56@gmail.com>
2024-12-23 15:17:36 +0600

    first commit

---
+++ restaurant.js
@@ -0,0 +1,6 @@
+let main-button = document .getElementById("main-button");
+let main-menu = document .getElementById("main-menu");
+
+main-button.addEventListener("click",() =>{
+    main-menu.classList.toggle("hidden");
+})
